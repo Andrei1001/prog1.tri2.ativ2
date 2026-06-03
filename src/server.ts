e69b542f9be723419c53e30943843ef52b11e689
Your Name <you@example.com>
2026-06-03 16:38:53 -0300

    Começamos o código

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+// FALAR SOBRE API REST
+
+const server = Bun.serve({
+    port: 3000,
+    routes: {
+        "/api-debugger": Bun.file(`public/api-debugger.html`),
+        "/test": {
+            GET: () => Response.json({ time: Date.now() }),
+            PUT: () => Response.json({ time: Date.now() }),
+            POST: () => Response.json({ time: Date.now() }),
+            DELETE: () => Response.json({ time: Date.now() })
+        },
+        fetch() {
+            return new Response("Not Found", { status: 404 })
+        }
+
+    }
+})
+console.log(`⚡ https://localhost:${server.port}`)
